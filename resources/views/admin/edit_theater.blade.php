bada8ffafca486d5a1bf53d1d9bb1edfdbbb7e97
victor141516 <yomellamovictor@gmail.com>
2017-01-03 17:35:08 +0100

    stuff

--- resources/views/admin/edit_theater.blade.php
+++ resources/views/admin/edit_theater.blade.php
@@ -61,44 +61,5 @@
 @endsection
 
 @section('extra-js')
-    <script type="text/javascript">
-        $(document).ready(() => {
-            if ($('#rows').val()) {
-                $('#columns').prop('disabled', false);
-            }
-            fillRows();
-            fillColumns();
-        });
-
-        $('#rows').on('input', () => {
-            $('#columns').val('');
-            fillRows();
-            if ($('#rows').val()) {
-                $('#columns').prop('disabled', false);
-            } else {
-                $('#columns').prop('disabled', true);
-            }
-        });
-        $('#columns').on('input', () => {
-            fillColumns();
-        });
-
-        function fillRows() {
-            $('.fila').remove();
-            $('#columns').val('');
-            var nFilas = $('#rows').val();
-            for (var i = 0; i < nFilas; i++) {
-                $('#theater').append('<div class="row fila" id="fila' + i + '"></div>');
-            }
-        }
-
-        function fillColumns() {
-            var nColumnas = $('#columns').val();
-            $('.fila').each((index, el) => {
-                for (var i = 0; i < nColumnas; i++) {
-                    $('<div class="text-center columna" id="columna' + i + '"><img src="http://88.8.199.15:81/Imagenes/Libre_1.bmp" alt=""></div>').appendTo(el);
-                }
-            });
-        }
-    </script>
+    <script type="text/javascript" src="js/display-seats.js"></script>
 @endsection
