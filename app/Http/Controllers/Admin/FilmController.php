cb45643e6794af6985c0391178cf015364e1bc08
victor141516 <yomellamovictor@gmail.com>
2017-01-10 13:47:28 +0100

    stuff

--- app/Http/Controllers/Admin/FilmController.php
+++ app/Http/Controllers/Admin/FilmController.php
@@ -66,7 +66,7 @@ class FilmController extends Controller
 
         return view('admin.film.report')
             ->withGroup($group)
-            ->withOrdernations(Schema::getColumnListing('tickets');)
+            ->withOrdenations(Schema::getColumnListing('tickets');)
             ->withFilms($films);
     }
 }
