d35ffdbac6a7e1cd1034354685c4e7f57a7cf89d
guille <darkwilly92@gmail.com>
2017-01-10 12:55:09 +0100

    wip

--- resources/views/admin/common/navigation.blade.php
+++ resources/views/admin/common/navigation.blade.php
@@ -58,13 +58,13 @@
                         Informes <span class="caret"></span>
                     </a>
                     <ul class="dropdown-menu">
-                        <li role="presentation" @if (Request::has('admin/films_report')) class="active" @endif>
+                        <li role="presentation" @if (Request::is('admin/films_report/*')) class="active" @endif>
                             <a href="{{ url('admin/films_report/genre') }}">Informe de películas</a>
                         </li>
                         <li role="presentation" @if (Request::is('admin/theater_report')) class="active" @endif>
                             <a href="{{ url('admin/theater_report') }}">Informe de salas</a>
                         </li>
-                        <li role="presentation" @if (Request::has('admin/tickets_report')) class="active" @endif>
+                        <li role="presentation" @if (Request::is('admin/tickets_report/*')) class="active" @endif>
                             <a href="{{ url('admin/tickets_report/projection_id') }}">Informe de entradas</a>
                         </li>
                     </ul>
